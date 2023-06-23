bd0d91ca416904623aa03a1487c2c8631fe21858
anuevo <adrian.nuevo@solera.com>
2023-06-23 16:53:15 +0200

    feat: Add routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,22 @@
 import styles from './App.module.css'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
 
     <>
-    
+
       <main className={styles.App}>
+
+        <Routes>
+
+          <Route path="/" element={<Container title={"Solera Teams - Bootcamp 5"} />}>
+            <Route index element={<CardList />} />
+            <Route path="team/:id" element={<TeamScore />} />
+          </Route>
+          
+        </Routes>
 
       </main>
 
--- Client/src/components/Container/Container.jsx
+++ Client/src/components/Container/Container.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import styles from './Container.module.css'
+import Logo from './../../assets/Logo.png'
 
 const Container = () => {
+
   return (
-    <div>Container</div>
+    
+    <div className={styles.Container}>
+
+      <div className={styles.Header}>
+
+        <h1> drqwqw </h1>
+
+        <img className={styles.Logo} src={Logo} alt='Logo Solera'/>
+
+      </div>
+            
+    </div>
+
   )
 }
 
