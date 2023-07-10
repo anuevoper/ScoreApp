22b62addeed06444b552d539fa277b6eb3c0e64c
Kenya Gonzalez <kenya.gonzalez@solera.com>
2023-07-10 14:30:04 +0200

    Added "Activity" class to be able to link activities to Teams.

---
+++ Server/src/main/java/com/example/MongoDBConn/model/Activity.java
@@ -0,0 +1,15 @@
+package com.example.MongoDBConn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Activity {
+    private String name;
+    private int score;
+}
--- Server/src/main/java/com/example/MongoDBConn/model/Team.java
+++ Server/src/main/java/com/example/MongoDBConn/model/Team.java
@@ -5,6 +5,9 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 @Setter
@@ -12,45 +15,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Team {
 	@Id
 	private String id;
+
 	private String name;
-	private String participants;
-	private String activities;
 
-	/****************************************
-	 * Refactored with project lombok for simplicity
-	 ****************************************************/
+	private List<String> participants;
 
-/*	public Team (String id, String name, String participants, String activities) {
-		super();
-		this.id = id;
-		this.setName(name);
-		this.setParticipants(participants);
-		this.setActivities(activities);
-		
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getParticipants() {
-		return participants;
-	}
-
-	public void setParticipants(String participants) {
-		this.participants = participants;
-	}
-
-	public String getActivities() {
-		return activities;
-	}
-	public void setActivities(String activities) {
-		this.activities = activities;
-	}*/
-	
-	
+	private List<Activity> activities;
 }
